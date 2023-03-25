ecb39265c66dee59bfbf826caeb60c15f3c96cfb
ericyilin8 <ericyilin8@gmail.com>
2023-03-25 16:24:43 -0400

    update connection

--- model/database.php
+++ model/database.php
@@ -1,7 +1,7 @@
 <?php
-    $dsn = 'mysql:host=localhost;dbname=zippyusedautos';
-    $username = 'root';
-    $password = '';
+    $dsn = 'mysql:host=s465z7sj4pwhp7fn.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=cf92s5dn4q6jkov1';
+    $username = 'vvff5qwejsq5djcj';
+    $password = 's3t41rzl0gkg04b0';
 
     try {
         $db = new PDO($dsn, $username, $password);
